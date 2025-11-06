caa609f3ac566e219a8050b428b139063018faa3
MertNaci <mertnaci38@gmail.com>
2025-11-06 17:26:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    // oyun başladı mı bilgisini tutan state
+    const [gameStarted, setGameStarted] = useState(false)
 
-  return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="card">
+                <h1>AI Tahmin Oyunu</h1>
+                <p>
+                    Üç görselden biri yapay zekâ tarafından üretilmiştir.<br/>
+                    Diğer ikisi gerçektir.<br/>
+                    Hangisinin yapay zekâ ürünü olduğunu bulabilir misin?
+                </p>
+                <button onClick={() => setGameStarted(true)}>
+                    Başla
+                </button>
+                <p>{String(gameStarted)}</p>
+            </div>
+        </>
+    )
 }
 
 export default App
