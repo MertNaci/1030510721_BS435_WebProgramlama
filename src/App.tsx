5846b6f74a41f72009792f81b94322605eec6954
MertNaci <mertnaci38@gmail.com>
2025-11-14 17:26:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,29 @@ function App() {
 
     return (
         <>
-            <div className="card">
-                <h1>AI Tahmin Oyunu</h1>
-                <p>
-                    Üç görselden biri yapay zekâ tarafından üretilmiştir.<br/>
-                    Diğer ikisi gerçektir.<br/>
-                    Hangisinin yapay zekâ ürünü olduğunu bulabilir misin?
-                </p>
-                <button onClick={() => setGameStarted(true)}>
-                    Başla
-                </button>
-                <p>{String(gameStarted)}</p>
-            </div>
+            {!gameStarted ? (
+                // Başlangıç ekranı
+                <div className="card">
+                    <h1>AI Tahmin Oyunu</h1>
+                    <p>
+                        Üç görselden biri yapay zekâ tarafından üretilmiştir.<br/>
+                        Diğer ikisi gerçektir.<br/>
+                        Hangisinin yapay zekâ ürünü olduğunu bulabilir misin?
+                    </p>
+                    <button onClick={() => setGameStarted(true)}>
+                        Başla
+                    </button>
+                    <p>{String(gameStarted)}</p>
+                </div>
+            ) : (
+                // Oyun ekranı
+                <div className="card">
+                    <h1>Oyun Başladı!</h1>
+                    <p>Burada görseller yer alacak.</p>
+                    <button onClick={() => setGameStarted(false)}>Başa Dön</button>
+                </div>
+            )
+            }
         </>
     )
 }
